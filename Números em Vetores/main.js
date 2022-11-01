8cd0a7e59783ddcb4ea69fdb7d53d14632ca18f9
camagaton <camagaton@gmail.com>
2022-11-01 15:17:07 -0300

    função realizada, falta descobrir como usar o gets e print no javascript

--- Números em Vetores/main.js
+++ Números em Vetores/main.js
@@ -1,3 +1,15 @@
 let vetor = [];
 let pares = [];
 let impares = [];
+
+for (let index = 0; index < vetor.length; index++) {
+    if(vetor[index] % 2 === 0){
+        pares.push(vetor[index])
+    }else{
+        impares.push(vetor[index])
+    }
+}
+    console.log('vetor: '+vetor)
+    console.log('pares: '+pares)
+    console.log('impares: '+impares)    
+
